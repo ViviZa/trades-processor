93a066f0c42c31f2d23336ae0c2ca2299c2ffd33
Vivian <vivian.zaddach@student.htw-berlin.de>
2020-10-20 09:06:49 +0200

    Initialize project

---
+++ src/main/java/com/tradesprocessor/tradesprocessor/TradesProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.tradesprocessor.tradesprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradesprocessor/tradesprocessor/TradesProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradesprocessor.tradesprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
